747bc5adcfba77754b0675c91b637f0f1ec2f156
Volodymyr <inc8877@gmail.com>
2021-03-18 15:24:55 +0200

    added overloads to WaitForAction

--- Runtime/WaitingForCondition.cs
+++ Runtime/WaitingForCondition.cs
@@ -1,3 +1,17 @@
+/*Copyright 2021 Volodymyr Bozhko
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+limitations under the License.*/
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,12 +23,41 @@ namespace ThreadMaster
     public static partial class Multithreading
     {
         /// <summary>
+        /// Invokes 'condition' while it returns false and performs the 'react' action as soon as 'condition' returns true.
+        /// <remarks>If you work with a backend, then use <see cref="WaitForCondition(Action, Func{bool}, int)"/>
+        /// overload with a polling delay, so as not to load the backend with requests </remarks>
+        /// </summary>
+        /// <param name="react">Invokes as soon as the 'condition' returns true</param>
+        /// <param name="condition">The condition under which the 'exit' action would perform</param>
+        /// <returns></returns>
+        public static async UniTask WaitForCondition(Action react, Func<bool> condition)
+        {
+            while (!condition.Invoke()) await UniTask.Yield();
+            react.Invoke();
+        }
+        
+        /// <summary>
+        /// Invokes 'condition' with a polling delay and performs the 'react' action as soon as 'condition' returns true
+        /// </summary>
+        /// <param name="react">Invokes as soon as the 'condition' returns true</param>
+        /// <param name="condition">The condition under which the 'exit' action would perform</param>
+        /// <param name="pollingDelay">The time between 'condition' invocations. Specified in milliseconds</param>
+        /// <returns></returns>
+        public static async UniTask WaitForCondition(Action react, Func<bool> condition, int pollingDelay)
+        {
+            while (!condition.Invoke())
+            {
+                await UniTask.Delay(pollingDelay);
+                await UniTask.Yield();
+            }
+            react.Invoke();
+        }
+        
+        /// <summary>
         /// Perform the '<code>init</code>' action immediately after invocation.
         /// Invokes 'condition' while it returns false and performs the 'exit' action as soon as 'condition' returns true.
-        /// <remarks>Be careful! This method polls the 'condition' func as often as it can,
-        /// it can be bad for the network if you are working with a server.
-        /// If traffic is important to you, use <see cref="WaitForCondition(Action, Action, Func{bool}, int)"/>
-        /// overload with a polling delay</remarks>
+        /// <remarks>If you work with a backend, then use <see cref="WaitForCondition(Action, Action, Func{bool}, int)"/>
+        /// overload with a polling delay, so as not to load the backend with requests </remarks>
         /// </summary>
         /// <param name="init"></param>
         /// <param name="exit"></param>
